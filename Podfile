07b4e3e0a287595acc77f124c3a8279542a11ce4
mothule <mothule.dev+github@gmail.com>
2023-09-08 15:43:43 +0900

    Initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '16.4'
+
+target 'ios-auth-flow-sample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ios-auth-flow-sample
+
+end
---
+++ ios-auth-flow-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-auth-flow-sample
+//
+//  Created by Motoki Kawakami on 2023/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
